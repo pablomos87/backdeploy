16076dacaa33e1564fc5dc10f8aa17efcaeb257f
pablomos <mosconpablo@gmail.com>
2023-12-09 11:13:25 -0300

    last commi sday

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://frontdeploy-gold.vercel.app'],
+  origin: ['http://localhost:3000', 'https://frontdeploy-pablomos87.vercel.app'],
   credentials: true,
 }));
 
