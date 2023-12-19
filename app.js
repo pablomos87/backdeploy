2f8d4844ab7ed3cb032f094113faf0c376245fe6
pablomos <mosconpablo@gmail.com>
2023-12-19 18:15:46 -0300

    commit 18 31

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger('combined'));
 app.use(express.json());
 app.use(cors({
 origin: ['https://frontdeploy-pablomos87.vercel.app', 'https://frontdeploy-gold.vercel.app', 'https://frontdeploy-git-master-pablomos87.vercel.app', 'https://proyectofinal-utn.netlify.app'],
-withCredentials: false,
+withCredentials: true,
 optionsSuccessStatus: 200 
 })); 
 
