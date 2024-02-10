fd38849932287314126ceea40e00c73397c9458f
pablomos <mosconpablo@gmail.com>
2024-02-10 10:38:42 -0300

    02/10

--- routers/visitsRouter.js
+++ routers/visitsRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const visitsRouter = express.Router();
 const { visitsCounter } = require('../dao/controllers/visitsController');
+const authenticateAdminToken = require ('../middleweres/tokenMiddlewere');
 
 visitsRouter.get('/count', authenticateAdminToken, visitsCounter);
 
