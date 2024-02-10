a2cca35fb4f4fa203e50c43df2d284b69f509d4e
pablomos <mosconpablo@gmail.com>
2024-02-10 11:02:21 -0300

    commit 10/02

--- routers/visitsRouter.js
+++ routers/visitsRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const visitsRouter = express.Router();
 const { visitsCounter } = require('../dao/controllers/visitsController');
+const authenticateAdminToken = require ('../middleweres/tokenMiddlewere');
 
-
-visitsRouter.get('/count', visitsCounter);
+visitsRouter.get('/count', authenticateAdminToken, visitsCounter);
 
 module.exports = visitsRouter;
